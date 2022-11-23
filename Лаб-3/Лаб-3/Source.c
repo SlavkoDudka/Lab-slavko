1e1f9337ec4f4cc26a27e55da1bc714ae4be9834
SlavkoDudka <114617260+SlavkoDudka@users.noreply.github.com>
2022-11-24 00:11:37 +0200

    1

---
+++ Лаб-3/Лаб-3/Source.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<math.h>
+
+ /*int main() { // 1.A
+
+	int f, r, e;
+
+	printf("Enter f, r:\n");
+	scanf_s("%d, %d", &f, &r);
+
+	if (f==5)
+	{
+		e=3*f+2*r;
+		printf("e=%d\n", e);
+	}
+	
+	if (f==0)
+	{
+		e = sqrt((8*r)/2*f-4);
+		printf("e=%d\n", e);
+	}
+
+	if (f==-5)
+	{
+		e=f*r-9;
+		printf("e=%d\n", e);
+	}
+       else
+	{
+		printf("it is not possible to evaluate the value of the expression");
+	}
+       return 0;
+} */
+
+/*int main() { //1.Á)
+
+	int f, r, e;
+
+
+	printf("Enter f, r:\n");
+	scanf_s("%d, %d", &f, &r);
+
+	if (f==5)
+	{
+		e = 3 * f + 2 * r;
+		printf("e=%d\n", e);
+	}
+
+	else if (f==0)
+	{
+		e = sqrt((8 * r) / 2 * f - 4);
+		printf("e=%d\n", e);
+	}
+
+	else if (f==-5)
+	{
+		e = f * r - 9;
+		printf("e=%d\n", e);
+	}
+
+
+	else
+	{
+		printf("it is not possible to evaluate the value of the expression");}
+	return 0;
+}*/
+
+
