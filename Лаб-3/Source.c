1bd468a66def73ca77a279cf4922e00316df7105
SlavkoDudka <114617260+SlavkoDudka@users.noreply.github.com>
2022-12-01 02:31:42 +0200

    lab-3.3

---
+++ Лаб-3/Source.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+	int x, y, z;
+	double t;
+	printf("Set values of integer variables for calculations:");
+	printf("\nx = ");
+	scanf_s("%d", &x);
+	printf("\ny = ");
+	scanf_s("%d", &y);
+	printf("\nz = ");
+	scanf_s("%d", &z);
+
+	
+	if ((3 * (x / z - 2 * y)) < 0)
+		printf("\nIt is not possible to solve the root of a negative number");
+	else if (z - 2 * y == 0)
+			printf("\nIt is impossible to solve, division by zero");
+	
+	else {
+		t = sqrt(3 * ((double)x / (double)z - 2 * (double)y)) - (double)y * (double)z + pow(sin(x), 2);
+		
+		printf("\nResult:%.2f", t);
+	}
+
+	return 0;
+}
